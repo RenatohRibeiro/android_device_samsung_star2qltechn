140997df254039b16040d79caaa584b7aee28c49
renatohribeiro <renatohribeh@outlook.com>
2021-02-17 22:48:41 +0000

    Pixel Experience bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,22 +1,12 @@
 #
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_star2qltechn.mk
+    $(LOCAL_DIR)/aosp_star2qltechn.mk
 
 COMMON_LUNCH_CHOICES := \
-	lineage_star2qltechn-user \
-	lineage_star2qltechn-userdebug \
-	lineage_star2qltechn-eng
+	aosp_star2qltechn-user \
+	aosp_star2qltechn-userdebug \
+	aosp_star2qltechn-eng
---
+++ aosp_star2qltechn.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/samsung/star2qltechn/full_star2qltechn.mk)
+
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+PRODUCT_NAME := aosp_star2qltechn
+BOARD_VENDOR := samsung
+
