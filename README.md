4979b7d1ffc2e0dc91b3cf0b91e831fc4afa5ce3
renatohribeiro <renatohribeh@outlook.com>
2021-02-18 15:37:49 +0000

    POSP bringup

--- README.md
+++
@@ -1,1 +0,0 @@
-android_device_samsung_star2qltechn
---
+++ potato_star2qltechn.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/samsung/star2qltechn/full_star2qltechn.mk)
+
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
+
+PRODUCT_NAME := potato_star2qltechn
+BOARD_VENDOR := samsung
+
